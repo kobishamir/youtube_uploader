37142243d28c3d9c43ed0e9758252d0b30a48f19
kobishamir <37914417+kobishamir@users.noreply.github.com>
2023-01-14 22:18:54 +0200

    Updated search_music_files.py and tried another kind of code

--- search_music_files.py
+++ search_music_files.py
@@ -2,19 +2,26 @@
 # This code search music files in a given directory, including sub-directories
 
 import os
+import subprocess
 
 path = "E:\המוזיקה שלי"
 
-
 def find_files_and_subfolders(path):
-    files = os.listdir(path)
-    for item in files:
-        if os.path.isdir(os.path.join(path, item)):
-            find_files_and_subfolders(os.path.join(path, item))
-        else:
-            if item.endswith('.mp3'):
-                # print(os.path.join(path, item)) #print path of file
-                print(item) # print name of file
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            if filename.endswith('.mp3'):
+                # Search for the music file on YouTube
+                # video = search_video(filename)
+                print(filename)
+                video = subprocess.run(["python", "search_video.py", "filename"])
+                if video is not None:
+                    # Get the name of the directory
+                    playlist_name = os.path.basename(dirpath)
+                    # Create a new playlist if it doesn't exist
+                    if playlist_name not in playlists:
+                        playlists[playlist_name] = create_playlist(playlist_name)
+                    # Add the video to the playlist
+                    add_to_playlist(playlists[playlist_name], video)
 
 
 find_files_and_subfolders(path)
